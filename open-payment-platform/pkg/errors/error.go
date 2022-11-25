6bfa15ae038ce8c5246629cc84e4d75ca0d046f0
baiyutang <irichardwang@gmail.com>
2022-11-25 10:42:33 +0800

    chore(pkg): remove unused code

--- open-payment-platform/pkg/errors/error.go
+++ open-payment-platform/pkg/errors/error.go
@@ -1,12 +1,1 @@
 package errors
-
-import "fmt"
-
-type Error struct {
-	Code int64
-	Msg  string
-}
-
-func (e *Error) Error() string {
-	return fmt.Sprintf("error: code = %d message = %s", e.Code, e.Msg)
-}
