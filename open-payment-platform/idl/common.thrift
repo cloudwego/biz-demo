68e1fd4291e49f8b9facee39ba62eb9aa363af95
baiyutang <irichardwang@gmail.com>
2022-12-09 10:37:12 +0800

    chore(common.thrift)

--- open-payment-platform/idl/common.thrift
+++ open-payment-platform/idl/common.thrift
@@ -3,7 +3,7 @@ namespace go common
 enum Err
 {
 	// gateway 10001- 19999
-    BadRequest        i32    = 10001,
+    BadRequest            = 10001,
 	Unauthorized          = 10002,
 	ServerNotFound        = 10003,
 	ServerMethodNotFound  = 10004,
