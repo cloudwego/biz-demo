84f89099f17348b74b78799765fce3dd252e56cc
Lorain <87760338+justlorain@users.noreply.github.com>
2023-01-02 19:03:32 +0800

    feat: replace requestid with traceid (#18)

--- easy_note/pkg/consts/consts.go
+++ easy_note/pkg/consts/consts.go
@@ -16,20 +16,21 @@
 package consts
 
 const (
-	NoteTableName   = "note"
-	UserTableName   = "user"
-	SecretKey       = "secret key"
-	IdentityKey     = "id"
-	Total           = "total"
-	Notes           = "notes"
-	ApiServiceName  = "demoapi"
-	NoteServiceName = "demonote"
-	UserServiceName = "demouser"
-	MySQLDefaultDSN = "gorm:gorm@tcp(localhost:3306)/gorm?charset=utf8&parseTime=True&loc=Local"
-	TCP             = "tcp"
-	UserServiceAddr = ":9000"
-	NoteServiceAddr = ":10000"
-	ExportEndpoint  = ":4317"
-	ETCDAddress     = "127.0.0.1:2379"
-	DefaultLimit    = 10
+	NoteTableName    = "note"
+	UserTableName    = "user"
+	SecretKey        = "secret key"
+	HeaderXRequestID = "X-Request-ID"
+	IdentityKey      = "id"
+	Total            = "total"
+	Notes            = "notes"
+	ApiServiceName   = "demoapi"
+	NoteServiceName  = "demonote"
+	UserServiceName  = "demouser"
+	MySQLDefaultDSN  = "gorm:gorm@tcp(localhost:3306)/gorm?charset=utf8&parseTime=True&loc=Local"
+	TCP              = "tcp"
+	UserServiceAddr  = ":9000"
+	NoteServiceAddr  = ":10000"
+	ExportEndpoint   = ":4317"
+	ETCDAddress      = "127.0.0.1:2379"
+	DefaultLimit     = 10
 )
