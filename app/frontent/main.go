892cf63833e45b81180fe6a2c3665b6b5915e900
rogerogers <rogers@rogerogers.com>
2023-12-05 23:16:23 +0800

    feat: template
    Signed-off-by: rogerogers <rogers@rogerogers.com>

--- app/frontent/main.go
+++ app/frontent/main.go
@@ -2,10 +2,12 @@ package main
 
 import (
 	"context"
+	"fmt"
 	"github.com/cloudwego/hertz/pkg/app"
 	"github.com/cloudwego/hertz/pkg/app/server"
 	"github.com/cloudwego/hertz/pkg/common/hlog"
 	"github.com/cloudwego/hertz/pkg/common/utils"
+	"github.com/cloudwego/hertz/pkg/protocol/consts"
 	"net/http"
 	"time"
 )
@@ -16,9 +18,44 @@ func main() {
 	h.LoadHTMLGlob("template/*")
 	h.Delims("{{", "}}")
 	h.GET("/", func(ctx context.Context, c *app.RequestContext) {
+		var items []map[string]string
+		for i := 1; i <= 10; i++ {
+			items = append(items, map[string]string{
+				"title":       fmt.Sprintf("product%d", i),
+				"description": fmt.Sprintf("product description %d", i),
+			})
+		}
 		c.HTML(http.StatusOK, "home", utils.H{
-			"title": "hello cloudwego",
+			"title":    "hello cloudwego",
+			"cart_num": 10,
+			"items":    items,
 		})
+	})
+	h.GET("/category", func(ctx context.Context, c *app.RequestContext) {
+		c.HTML(consts.StatusOK, "category", utils.H{
+			"title": "category",
+		})
+	})
+	h.GET("/product", func(ctx context.Context, c *app.RequestContext) {
+		c.HTML(consts.StatusOK, "product", utils.H{
+			"title": "product",
+		})
+	})
+	h.GET("/cart", func(ctx context.Context, c *app.RequestContext) {
+		c.HTML(consts.StatusOK, "cart", utils.H{
+			"title": "cart",
+		})
+	})
+	h.GET("/order", func(ctx context.Context, c *app.RequestContext) {
+		c.HTML(consts.StatusOK, "order", utils.H{
+			"title": "order",
+		})
+	})
+	h.GET("/about", func(ctx context.Context, c *app.RequestContext) {
+		c.HTML(consts.StatusOK, "about", utils.H{
+			"title": "about",
+		})
+
 	})
 	h.Static("/static", "./")
 	h.Spin()
