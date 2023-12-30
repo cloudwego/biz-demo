be298377f4700f2d83a234c85d371f86e4a23012
baiyutang <irichardwang@gmail.com>
2023-12-30 12:37:21 +0800

    feat(frontend):logout

--- app/frontend/biz/handler/auth/auth_service.go
+++ app/frontend/biz/handler/auth/auth_service.go
@@ -61,11 +61,12 @@ func Logout(ctx context.Context, c *app.RequestContext) {
 		return
 	}
 
-	resp, err := service.NewLogoutService(ctx, c).Run(&req)
+	_, err = service.NewLogoutService(ctx, c).Run(&req)
 	if err != nil {
 		utils.SendErrResponse(ctx, c, consts.StatusOK, err)
 		return
 	}
+	redirect := "/"
 
-	utils.SendSuccessResponse(ctx, c, consts.StatusOK, resp)
+	c.Redirect(consts.StatusFound, []byte(redirect))
 }
--- app/frontend/biz/service/logout.go
+++ app/frontend/biz/service/logout.go
@@ -5,6 +5,7 @@ import (
 
 	common "github.com/baiyutang/gomall/app/frontend/hertz_gen/frontend/common"
 	"github.com/cloudwego/hertz/pkg/app"
+	"github.com/hertz-contrib/sessions"
 )
 
 type LogoutService struct {
@@ -17,10 +18,8 @@ func NewLogoutService(Context context.Context, RequestContext *app.RequestContex
 }
 
 func (h *LogoutService) Run(req *common.Empty) (resp *common.Empty, err error) {
-	//defer func() {
-	// hlog.CtxInfof(h.Context, "req = %+v", req)
-	// hlog.CtxInfof(h.Context, "resp = %+v", resp)
-	//}()
-	// todo edit your code
+	session := sessions.Default(h.RequestContext)
+	session.Clear()
+	session.Save()
 	return
 }
